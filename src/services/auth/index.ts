df45112242a30bce3e3ecd7ac4805782de2033f1
bonaxl015 <bonaxl015@gmail.com>
2025-09-20 12:51:09 +0800

    feat(register): adding register screen along with functionalities

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,11 +1,27 @@
 import apiClient from '@services/apiClient';
 import { AxiosError } from 'axios';
-import { LoginFormData } from './types';
+import { LoginFormData, RegisterFormData } from './types';
 import { AUTH_API_URL } from './url';
 
-export default async function submitLoginForm(data: LoginFormData) {
+export async function submitLoginForm(data: LoginFormData) {
 	try {
 		const res = await apiClient.post(AUTH_API_URL.LOGIN, data);
+
+		if (!res.data && res.status !== 200) {
+			throw new Error(res.data.message);
+		}
+
+		return { token: res.data.token };
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			throw new Error(error.message);
+		}
+	}
+}
+
+export async function submitRegistrationForm(data: RegisterFormData) {
+	try {
+		const res = await apiClient.post(AUTH_API_URL.REGISTER, data);
 
 		if (!res.data && res.status !== 200) {
 			throw new Error(res.data.message);
--- src/services/auth/types.ts
+++ src/services/auth/types.ts
@@ -2,3 +2,9 @@ export type LoginFormData = {
 	email: string;
 	password: string;
 };
+
+export type RegisterFormData = {
+	name: string;
+	email: string;
+	password: string;
+};
