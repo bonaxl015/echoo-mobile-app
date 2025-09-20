ea103c430aa1246330a6fb50c41926a031aac648
bonaxl015 <bonaxl015@gmail.com>
2025-09-20 12:53:12 +0800

    feat(register): added error display message

--- src/components/FormTextInput/index.tsx
+++ src/components/FormTextInput/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Control, Controller, FieldPath, FieldValues, RegisterOptions } from 'react-hook-form';
-import { TextInput, useTheme } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Text, TextInput, useTheme } from 'react-native-paper';
 
 interface IFormTextInput {
 	control: Control<FieldValues, any, FieldValues>;
@@ -30,25 +31,40 @@ export default function FormTextInput({
 			name={name}
 			rules={rules}
 			render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
-				<TextInput
-					label={label}
-					value={value}
-					onChangeText={onChange}
-					onBlur={onBlur}
-					secureTextEntry={secureTextEntry}
-					mode="outlined"
-					error={!!error}
-					style={{ marginBottom: 0 }}
-					placeholder={placeholder}
-					theme={{
-						colors: {
-							text: theme.colors.onSurface,
-							placeholder: theme.colors.onSurfaceVariant,
-							primary: error ? theme.colors.error : theme.colors.primary
-						}
-					}}
-				/>
+				<View style={{ marginBottom: 12 }}>
+					<TextInput
+						label={label}
+						value={value}
+						onChangeText={onChange}
+						onBlur={onBlur}
+						secureTextEntry={secureTextEntry}
+						mode="outlined"
+						error={!!error}
+						style={{ marginBottom: 0 }}
+						placeholder={placeholder}
+						theme={{
+							colors: {
+								text: theme.colors.onSurface,
+								placeholder: theme.colors.onSurfaceVariant,
+								primary: error ? theme.colors.error : theme.colors.primary
+							}
+						}}
+					/>
+
+					{error && (
+						<Text variant="labelSmall" style={[styles.errorText, { color: theme.colors.error }]}>
+							{error.message?.toString()}
+						</Text>
+					)}
+				</View>
 			)}
 		/>
 	);
 }
+
+const styles = StyleSheet.create({
+	errorText: {
+		marginTop: 4,
+		marginLeft: 4
+	}
+});
