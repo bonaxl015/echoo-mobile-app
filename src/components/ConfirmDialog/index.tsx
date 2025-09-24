2271377c72bc905a851a2d82f3cb7d0bf5c4fa44
bonaxl015 <bonaxl015@gmail.com>
2025-09-25 00:10:48 +0800

    feat(delete-post): confirm dialog common component

---
+++ src/components/ConfirmDialog/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button, Dialog, Portal, Text, useTheme } from 'react-native-paper';
+
+export interface IConfirmDialog {
+	title?: string;
+	content: string;
+	onCancel: () => void;
+	onConfirm: () => void;
+	confirmLabel?: string;
+	cancelLabel?: string;
+	dialogVisible: boolean;
+}
+
+export default function ConfirmDialog({
+	title,
+	content,
+	cancelLabel = 'Cancel',
+	confirmLabel = 'Confirm',
+	onCancel,
+	onConfirm,
+	dialogVisible
+}: IConfirmDialog) {
+	const theme = useTheme();
+
+	return (
+		<Portal>
+			<Dialog
+				visible={dialogVisible}
+				onDismiss={onCancel}
+				style={{ backgroundColor: theme.colors.background }}
+			>
+				{title && <Dialog.Title style={{ color: theme.colors.onSurface }}>{title}</Dialog.Title>}
+				<Dialog.Content>
+					<Text variant="bodyLarge" style={{ color: theme.colors.onSurface }}>
+						{content}
+					</Text>
+				</Dialog.Content>
+				<Dialog.Actions>
+					<Button onPress={onCancel} textColor={theme.colors.onSurfaceVariant} uppercase>
+						{cancelLabel}
+					</Button>
+					<Button onPress={onConfirm} textColor={theme.colors.primary} uppercase>
+						{confirmLabel}
+					</Button>
+				</Dialog.Actions>
+			</Dialog>
+		</Portal>
+	);
+}
