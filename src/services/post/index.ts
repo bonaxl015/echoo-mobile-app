366bed3234d6f554f61897c5bc22ac5907a625a6
bonaxl015 <bonaxl015@gmail.com>
2025-09-24 15:14:43 +0800

    feat(edit-post): update post service

--- src/services/post/index.ts
+++ src/services/post/index.ts
@@ -1,7 +1,13 @@
 import apiClient from '@services/apiClient';
 import dateFormatter from '@utils/dateFormatter';
 import { AxiosError } from 'axios';
-import { CreatePostResponse, ICreatePostService, PostResponse } from './types';
+import {
+	CreatePostResponse,
+	ICreatePostService,
+	IUpdatePostService,
+	PostResponse,
+	UpdatePostResponse
+} from './types';
 import { POST_API_URL } from './url';
 
 export async function getPostList({ pageParam = 1 }) {
@@ -37,6 +43,18 @@ export async function getPostList({ pageParam = 1 }) {
 export async function createPost(data: ICreatePostService) {
 	try {
 		const res = await apiClient.post<CreatePostResponse>(POST_API_URL.CREATE_POST, data);
+
+		return res.data;
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			throw new Error(error.message);
+		}
+	}
+}
+
+export async function updatePost(data: IUpdatePostService) {
+	try {
+		const res = await apiClient.patch<UpdatePostResponse>(POST_API_URL.UPDATE_POST, data);
 
 		return res.data;
 	} catch (error) {
--- src/services/post/url.ts
+++ src/services/post/url.ts
@@ -1,4 +1,5 @@
 export const POST_API_URL = {
 	GET_POST_LIST: '/api/v1/post/get-all',
-	CREATE_POST: '/api/v1/post/create'
+	CREATE_POST: '/api/v1/post/create',
+	UPDATE_POST: '/api/v1/post/update'
 } as const;
