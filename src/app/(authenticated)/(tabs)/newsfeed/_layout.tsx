768af0839bf8851221d235204a5e8a464ec1967c
bonaxl015 <bonaxl015@gmail.com>
2025-09-27 14:17:07 +0800

    feat(view-post): move to a new directory group

--- src/app/(authenticated)/(tabs)/newsfeed/_layout.tsx
+++ src/app/(authenticated)/(tabs)/newsfeed/_layout.tsx
@@ -1,8 +1,16 @@
 import { Stack } from 'expo-router';
+import { useTheme } from 'react-native-paper';
 
 export default function NewsfeedStackLayout() {
+	const theme = useTheme();
+
 	return (
-		<Stack>
+		<Stack
+			screenOptions={{
+				contentStyle: { backgroundColor: theme.colors.background },
+				animation: 'slide_from_right'
+			}}
+		>
 			<Stack.Screen
 				name="index"
 				options={{
