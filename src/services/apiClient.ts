77609fa6c840746864bf72ee423739399ac8bf56
bonaxl015 <bonaxl015@gmail.com>
2025-10-01 22:38:21 +0800

    feat(auth): interceptor to force logout on expired tokens

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,9 @@
 import { ENV } from '@config/env';
+import { STATUS_CODE } from '@constants/statusCodes';
 import { useAuthStore } from '@store/useAuthStore';
 import axios from 'axios';
+import { router } from 'expo-router';
+import Toast from 'react-native-toast-message';
 
 const apiClient = axios.create({
 	baseURL: ENV.BACKEND_URL,
@@ -18,5 +21,24 @@ apiClient.interceptors.request.use((config) => {
 
 	return config;
 });
+
+apiClient.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.response?.status === STATUS_CODE.UNAUTHORIZED) {
+			const logout = useAuthStore.getState().logout;
+
+			Toast.show({
+				type: 'error',
+				text1: 'Session expired',
+				text2: 'Please log in again'
+			});
+
+			logout();
+
+			router.replace('/auth/login');
+		}
+	}
+);
 
 export default apiClient;
