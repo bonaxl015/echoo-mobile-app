d9f24d36712b549af30738e58c8ad2f9f9f56cf1
bonaxl015 <bonaxl015@gmail.com>
2025-10-05 14:33:33 +0800

    feat(like-post): like post service

---
+++ src/services/like/index.ts
@@ -0,0 +1,71 @@
+import apiClient from '@services/apiClient';
+import { DEFAULT_PAGE_SIZE, DEFAULT_PAGE_SIZE_STRING } from '@services/constants';
+import dateFormatter from '@utils/dateFormatter';
+import { AxiosError } from 'axios';
+import {
+	PostLikeListRequest,
+	PostLikeListResponse,
+	PostLikeRequest,
+	PostLikeResponse,
+	PostUnlikeRequest,
+	PostUnlikeResponse
+} from './types';
+import { LIKE_API_URL } from './url';
+
+export async function getPostLikeList({ postId, pageParam = 1 }: PostLikeListRequest) {
+	try {
+		const params = {
+			postId,
+			pageNumber: pageParam.toString(),
+			pageSize: DEFAULT_PAGE_SIZE_STRING
+		};
+		const urlStringParams = new URLSearchParams(params);
+		const res = await apiClient.get<PostLikeListResponse>(
+			`${LIKE_API_URL.GET_POST_LIKES}?${urlStringParams.toString()}`
+		);
+
+		const dateFormattedLikes = res.data.likes.length
+			? res.data.likes.map((item) => ({
+					...item,
+					createdAt: dateFormatter(item.createdAt),
+					updatedAt: dateFormatter(item.updatedAt)
+				}))
+			: [];
+
+		return {
+			likes: dateFormattedLikes,
+			nextPage:
+				res.data.likes.length && res.data.likes.length === DEFAULT_PAGE_SIZE
+					? pageParam + 1
+					: undefined
+		};
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			throw new Error(error.message);
+		}
+	}
+}
+
+export async function likePost(data: PostLikeRequest) {
+	try {
+		const res = await apiClient.post<PostLikeResponse>(LIKE_API_URL.LIKE_POST, data);
+
+		return res.data;
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			throw new Error(error.message);
+		}
+	}
+}
+
+export async function unlikePost(data: PostUnlikeRequest) {
+	try {
+		const res = await apiClient.delete<PostUnlikeResponse>(LIKE_API_URL.LIKE_POST, { data });
+
+		return res.data;
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			throw new Error(error.message);
+		}
+	}
+}
