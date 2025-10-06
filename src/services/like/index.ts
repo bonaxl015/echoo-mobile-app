a53a18d08c90a10c7ffb5433acf9a239ccd994ac
bonaxl015 <bonaxl015@gmail.com>
2025-10-06 21:39:12 +0800

    feat(view-likes): view post and comment likes service and query hooks

--- src/services/like/index.ts
+++ src/services/like/index.ts
@@ -3,6 +3,8 @@ import { DEFAULT_PAGE_SIZE, DEFAULT_PAGE_SIZE_STRING } from '@services/constants
 import dateFormatter from '@utils/dateFormatter';
 import { AxiosError } from 'axios';
 import {
+	CommentLikeListRequest,
+	CommentLikeListResponse,
 	CommentLikeRequest,
 	CommentLikeResponse,
 	CommentUnlikeRequest,
@@ -26,6 +28,40 @@ export async function getPostLikeList({ postId, pageParam = 1 }: PostLikeListReq
 		const urlStringParams = new URLSearchParams(params);
 		const res = await apiClient.get<PostLikeListResponse>(
 			`${LIKE_API_URL.GET_POST_LIKES}?${urlStringParams.toString()}`
+		);
+
+		const dateFormattedLikes = res.data.likes.length
+			? res.data.likes.map((item) => ({
+					...item,
+					createdAt: dateFormatter(item.createdAt),
+					updatedAt: dateFormatter(item.updatedAt)
+				}))
+			: [];
+
+		return {
+			likes: dateFormattedLikes,
+			nextPage:
+				res.data.likes.length && res.data.likes.length === DEFAULT_PAGE_SIZE
+					? pageParam + 1
+					: undefined
+		};
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			throw new Error(error.message);
+		}
+	}
+}
+
+export async function getCommentLikeList({ commentId, pageParam = 1 }: CommentLikeListRequest) {
+	try {
+		const params = {
+			commentId,
+			pageNumber: pageParam.toString(),
+			pageSize: DEFAULT_PAGE_SIZE_STRING
+		};
+		const urlStringParams = new URLSearchParams(params);
+		const res = await apiClient.get<CommentLikeListResponse>(
+			`${LIKE_API_URL.GET_COMMENT_LIKES}?${urlStringParams.toString()}`
 		);
 
 		const dateFormattedLikes = res.data.likes.length
