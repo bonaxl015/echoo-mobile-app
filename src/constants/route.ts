20e15e6ca8ec741b2a0e8cf386b352528405d27d
bonaxl015 <bonaxl015@gmail.com>
2025-10-07 22:45:41 +0800

    feat(redirect-profile): update route path objects

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,2 +1,5 @@
-export const NEWSFEED_PATH = '/newsfeed';
-export const VIEW_POST_PATH = '/view-post';
+export const PATHS = {
+	NEWSFEED: '/(authenticated)/(tabs)/newsfeed',
+	VIEW_POST: '/(authenticated)/(full-screen)/view-post',
+	VIEW_USER_PROFILE: '/(authenticated)/(full-screen)/user-profile/[userId]'
+} as const;
