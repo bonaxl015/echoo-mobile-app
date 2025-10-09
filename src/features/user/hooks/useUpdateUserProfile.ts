fd598ac0d8815d37be75b09490858fa3cb0a6f60
bonaxl015 <bonaxl015@gmail.com>
2025-10-09 18:44:49 +0800

    feat(edit-profile): edit user mutations

--- src/features/user/hooks/useUpdateUserProfile.ts
+++ src/features/user/hooks/useUpdateUserProfile.ts
@@ -1,23 +1,35 @@
-import { updateUserProfile } from '@services/user';
+import { PATHS } from '@constants/route';
+import { updateUserProfile, updateUserProfilePhoto } from '@services/user';
 import { useAuthStore } from '@store/useAuthStore';
 import { useMutation } from '@tanstack/react-query';
+import { usePathname, useRouter } from 'expo-router';
 import Toast from 'react-native-toast-message';
 
 export function useUpdateUserProfile() {
 	const setUser = useAuthStore((s) => s.setUser);
+	const router = useRouter();
+	const pathname = usePathname();
 
-	return useMutation({
+	const redirectToSettingsTab = () => {
+		if (pathname !== PATHS.USER_SETTINGS_NORMALIZED) {
+			router.push(PATHS.USER_SETTINGS);
+		}
+	};
+
+	const updateUserProfileMutation = useMutation({
 		mutationFn: updateUserProfile,
 		onSuccess: async (data) => {
 			Toast.show({
 				type: 'success',
 				text1: 'Success',
-				text2: 'Post updated successfully'
+				text2: 'User data updated successfully'
 			});
 
 			if (data?.user) {
 				setUser(data.user);
 			}
+
+			redirectToSettingsTab();
 		},
 		onError: () => {
 			Toast.show({
@@ -27,4 +39,27 @@ export function useUpdateUserProfile() {
 			});
 		}
 	});
+
+	const updateUserProfilePhotoMutation = useMutation({
+		mutationFn: updateUserProfilePhoto,
+		onSuccess: async (data) => {
+			if (data?.user) {
+				setUser(data.user);
+			}
+
+			redirectToSettingsTab();
+		},
+		onError: () => {
+			Toast.show({
+				type: 'error',
+				text1: 'Error',
+				text2: 'Could not update user profile photo'
+			});
+		}
+	});
+
+	return {
+		updateUserProfileMutation,
+		updateUserProfilePhotoMutation
+	};
 }
