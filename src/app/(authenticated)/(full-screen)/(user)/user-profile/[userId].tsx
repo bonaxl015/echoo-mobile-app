69dcca6f5294be8518332f5d5983dcf0c2e8a799
Bon Axl Feeser <78939695+bonaxl015@users.noreply.github.com>
2025-10-13 22:32:13 +0800

    fix(user-profile): fix rendering issues in user profile (#42)

--- src/app/(authenticated)/(full-screen)/(user)/user-profile/[userId].tsx
+++ src/app/(authenticated)/(full-screen)/(user)/user-profile/[userId].tsx
@@ -1,7 +1,9 @@
 import { UserProfile } from '@features/user/components/UserProfile';
 import { useGetUserProfile } from '@features/user/hooks/useGetUserProfile';
 import { useLocalSearchParams } from 'expo-router';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 
 type UserProfileParams = {
 	userId: string;
@@ -10,18 +12,33 @@ type UserProfileParams = {
 export default function UserProfileScreen() {
 	const { userId } = useLocalSearchParams<UserProfileParams>();
 
-	const { data } = useGetUserProfile(userId);
+	const { data, isPending } = useGetUserProfile(userId);
 
-	if (!data) {
-		return null;
+	const user = useMemo(
+		() => ({
+			id: userId,
+			name: data?.user.name ?? '',
+			bio: data?.user.bio ?? '',
+			profilePhoto: data?.user.profilePhoto ?? ''
+		}),
+		[data, userId]
+	);
+
+	if (isPending) {
+		return (
+			<View style={styles.center}>
+				<ActivityIndicator size="large" />
+			</View>
+		);
 	}
-
-	const user = {
-		id: data.user.id,
-		name: data.user.name,
-		bio: data.user.bio,
-		profilePhoto: data.user.profilePhoto
-	};
 
 	return <UserProfile user={user} isFullScreenMode />;
 }
+
+const styles = StyleSheet.create({
+	center: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+});
